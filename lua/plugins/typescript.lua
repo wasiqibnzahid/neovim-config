c2b95583448af0f36413479233c34f1301a4c612
Wasiq Bin Zahid <wasiqbinzahid@teraception.com>
2024-01-18 16:00:37 +0500

    Removed ts-server

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -4,4 +4,17 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
   },
+  -- Disabled tsserver because we have pmizio-ts-tools and its faster - double intellisense was bothersome
+  {
+    "neovim/nvim-lspconfig",
+    {
+      opts = {
+        servers = {
+          tsserver = {
+            enabled = false,
+          },
+        },
+      },
+    },
+  },
 }
