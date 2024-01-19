1da62d25e147576b4c7f7d768cf9a795d9d0b673
Wasiq Bin Zahid <wasiqbinzahid@teraception.com>
2024-01-19 16:59:42 +0500

    Updated local config

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.lsp.set_log_level("debug")
+-- require("cmp").config.formatting = {
+--   format = require("tailwindcss-colorizer-cmp").formatter,
+-- }
--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+  -- {
+  --   "roobert/tailwindcss-colorizer-cmp.nvim",
+  --   -- optionally, override the default options:
+  --   config = function()
+  --     require("tailwindcss-colorizer-cmp").setup({
+  --       color_square_width = 2,
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       tailwindcss = {},
+  --     },
+  --   },
+  -- },
+}
