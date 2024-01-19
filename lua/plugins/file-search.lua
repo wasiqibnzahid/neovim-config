4d5ebe03d83ee0eb03dbaba70e7864dc7484872f
Wasiq Bin Zahid <wasiqbinzahid@teraception.com>
2024-01-20 00:14:37 +0500

    ADDED SEARCH CONFIG

---
+++ lua/plugins/file-search.lua
@@ -0,0 +1,8 @@
+return {
+  -- NOT NEEDED, RIPGREP is installed in the OS through executable or package manager - search will work after that
+  -- <leader> s g
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   dependencies = { "BurntSushi/ripgrep" },
+  -- },
+}
